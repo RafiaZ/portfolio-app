b185bfad99e9d0bb0fbb5efc831289e9caef1cd7
Rafia Zafar <rafiazafar75@gmail.com>
2021-08-13 17:02:09 +0500

    adding a use state hook to show the nav when hamburger clicked

---
+++ my-app/src/components/header/Burger.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../header/Burger.css'
+
+function Burger() {
+    return (
+        <div className="burger">
+            <span className="dash"></span>
+            <span className="dash"></span>
+            <span className="dash"></span>
+        </div>
+    )
+}
+
+export default Burger
+
--- my-app/src/components/header/Navbar.js
+++ my-app/src/components/header/Navbar.js
@@ -1,20 +1,24 @@
 import React from 'react'
+
+import Burger from '../header/Burger'
 import '../header/Navbar.css'
 
 function Navbar() {
     return (
         <div className="navbar">
+            
             <div className="logo">
                 <h3>Rafia</h3>
             </div>
             <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Skills</li>
-                <li>Resume</li>
-                <li>Portfolio</li>
-                <li>Contact</li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Skills</a></li>
+                <li><a href="#">Resume</a></li>
+                <li><a href="#">Portfolio</a></li>
+                <li><a href="#">Contact</a></li>
             </ul>
+            <Burger/>
         </div>
     )
 }
